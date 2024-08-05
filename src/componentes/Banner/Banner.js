4cd0e1cf0a8ed2266cd343ea4c07e1ceeee9f35e
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-08-05 19:21:21 -0300

    terminando front

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import "./banner.css";
+
+export const Banner = () => {
+  // JSX
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="O banner principal do Organo" />
+    </header>
+  );
+};
